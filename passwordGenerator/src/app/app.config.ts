48c7f9e83381114f4f95efbddd27d97df07fafa4
Koba_Yashy <149396138+Kobaashi@users.noreply.github.com>
2025-05-08 15:34:06 +0300

    initial project

---
+++ passwordGenerator/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom(HttpClientModule)
+  ]
+};
